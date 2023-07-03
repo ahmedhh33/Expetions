4db0bba11532f2fc896e3c04ac9c3fa41faeef2c
ahmedhh33 <ahmedhh3633@gmail.com>
2023-07-03 12:16:03 +0400

    multible exceptions

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Exeptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3,0 };
+            //foreach (int i in a) { Console.WriteLine(i); }
+            //Console.WriteLine(a[5]);//here will show exceptions
+
+            //Console.WriteLine(divide(4, 2));
+            //Console.WriteLine(divide(2, 2));
+            //Console.WriteLine(divide(3, 0));
+            try
+            {
+                Console.WriteLine(a[0] / a[2]);
+                Console.WriteLine(a[10] / a[3]);
+                Console.WriteLine(a[2] / a[3]);
+            } catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"divide by zero exeption");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Endex out of range");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //Console.WriteLine(a[1] / a[1]);
+        }
+
+        static float divide(int a,int b)
+        {
+            try
+            {
+                return a / b;
+            } catch (DivideByZeroException e)
+            {
+                return 0;
+            }
+        }
+    }
+}
